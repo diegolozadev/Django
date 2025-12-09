cd161f242691573b526451c1b4b52871af3e7230
Diego Lozano <diegolozadev@gmail.com>
2025-12-09 11:12:12 -0500

    Add Python files for higher-order functions and decorators with examples

---
+++ 02-fundamentos_python/13-decorators.py
@@ -0,0 +1,18 @@
+
+# Decoradores
+def require_auth(func):
+    def wrapper(user):
+        if user.lower() == "admin":
+            return func(user)
+        else:
+            return "Acceso denegado"
+    
+    return wrapper
+
+@require_auth # @ Este es el decorador
+def admin_dashboard(user):
+    return f"Bienvenido al panel, {user}"
+        
+
+print(admin_dashboard("Admin"))
+print(admin_dashboard("invitado"))
