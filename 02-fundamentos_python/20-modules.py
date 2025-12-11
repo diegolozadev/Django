2712252b42b4563674b305256a23b651fd2bc06e
Diego Lozano <diegolozadev@gmail.com>
2025-12-11 11:50:32 -0500

    Add Python files for bank account management, error handling, and file operations with examples

---
+++ 02-fundamentos_python/20-modules.py
@@ -0,0 +1,8 @@
+import math_utils
+from my_package import messages
+
+result = math_utils.addition(3,4)
+print(f"El resultado es: {result}")
+
+print(messages.greet("Diego"))
+print(messages.bye("Mafe"))
