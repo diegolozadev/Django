00e548c6363d568b2b79d70f972bb70333a3b1b2
Diego Lozano <diegolozadev@gmail.com>
2025-12-19 15:52:15 -0500

    Add functionality to handle day numbers and improve quote retrieval in views

--- playground/quotes/views.py
+++ playground/quotes/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 # Create your views here.
 
-def days_week(request, day):
-    quote_tex = None
-    if day == "monday":
-        quote_tex = "Pienso, luego existo"
-    elif day == "tuesday":
-        quote_tex = "La vida es bella"
-    elif day == "wednesday":
-        quote_tex = "Belleza de día"
-    else:
-        return HttpResponseNotFound("No hay frase para este día")
+days_of_week = {
+    "monday": "Pienso, luego existo",
+    "tuesday": "La vida es bella",
+    "wednesday": "Belleza de día",
+    "thursday": "Carpe diem",
+    "friday": "Vive y deja vivir",
+    "saturday": "El tiempo es oro",
+    "sunday": "Aprovecha el día",
+}
+
+def days_week_with_number(request, day):
+    days = list(days_of_week.keys())
+    if day > len(days):
+        return HttpResponseNotFound("El día no existe")
+    redirect_day = days[day-1]
+    return HttpResponseRedirect(f"/quotes/{redirect_day}")
     
-    return HttpResponse(quote_tex)
+    
+def days_week(request, day):
+    try:
+        quote_text = days_of_week[day]
+        return HttpResponse(quote_text)
+    except:
+        return HttpResponseNotFound("No hay frase para este día")
