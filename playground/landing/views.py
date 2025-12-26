23c96b1267694d5283cac81f77daea1a72a22d15
Diego <diegolozanodev@gmail.com>
2025-12-26 15:19:51 -0500

    Enhance landing page with personalized greetings and technology stack display; add stack detail view

--- playground/landing/views.py
+++ playground/landing/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import date
 
 # Create your views here.
 
 def home(request):
-    return render(request, "landing/landing.html")
+    today = date.today()
+    stack = [
+            {'id': 'python', 'name': 'Python'},
+            {'id': 'django', 'name': 'Django'},
+            {'id': 'react', 'name': 'React'},
+            {'id': 'php', 'name': 'PHP'},
+            ]
+
+    return render(request, "landing/landing.html", {
+        "name": "fernando",
+        "age": 28,
+        "today": today,
+        "stack": stack,
+    })
+
+def stack_detail(request, tool):
+    return HttpResponse(f"Tecnología: {tool}")
